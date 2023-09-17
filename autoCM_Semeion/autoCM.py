1a5163932d8cc7f8b6c66cb4179e26817b1455dc
Ishikawa7 <davidegrimaldi92@gmail.com>
2023-09-17 11:36:29 +0200

    fixing imports

--- autoCM_Semeion/autoCM.py
+++ autoCM_Semeion/autoCM.py
@@ -2,9 +2,10 @@ import requests
 import pandas as pd
 from io import StringIO
 
-def autoCM_fit(dataset : pd.Dataframe) -> pd.DataFrame:
+def autoCM_fit(dataset : pd.DataFrame) -> pd.DataFrame:
     url = "https://us-central1-davidesemeion.cloudfunctions.net/function-1"
     df_json = dataset.to_json()
     response = requests.post(url, json = df_json)
+    print(response)
     df_result = pd.read_json(StringIO(response.json))
     return df_result
