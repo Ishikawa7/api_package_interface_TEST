087939b3d947a8f2d1313fe43aec05d6ae8556ad
Ishikawa7 <davidegrimaldi92@gmail.com>
2023-09-17 15:22:15 +0200

    finished

--- autoCM_Semeion/autoCM.py
+++ autoCM_Semeion/autoCM.py
@@ -7,12 +7,7 @@ def autoCM_fit(dataset : pd.DataFrame) -> pd.DataFrame:
     df_json = dataset.to_json()
     response = requests.post(url, json = df_json)
     # check status code
-    print("PRINT 1")
-    print(response.status_code)
+    print("Status response: " + str(response.status_code))
     assert response.status_code == 200
-    #print("PRINT 2")
-    #print(response)
-    #print("PRINT 3")
-    #print(response.json())
     df_result = pd.read_json(StringIO(response.json()))
     return df_result
