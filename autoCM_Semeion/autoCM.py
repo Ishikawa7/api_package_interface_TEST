493ccbd25eec08647b04102f72447cc4f57dd65e
Ishikawa7 <davidegrimaldi92@gmail.com>
2023-09-17 12:12:03 +0200

    validating response

--- autoCM_Semeion/autoCM.py
+++ autoCM_Semeion/autoCM.py
@@ -6,6 +6,13 @@ def autoCM_fit(dataset : pd.DataFrame) -> pd.DataFrame:
     url = "https://us-central1-davidesemeion.cloudfunctions.net/function-1"
     df_json = dataset.to_json()
     response = requests.post(url, json = df_json)
-    print(response)
-    df_result = pd.read_json(StringIO(response.json))
+    # check status code
+    print("PRINT 1")
+    print(response.status_code)
+    assert response.status_code == 200
+    #print("PRINT 2")
+    #print(response)
+    #print("PRINT 3")
+    #print(response.json())
+    df_result = pd.read_json(StringIO(response.json()))
     return df_result
